b61880120a0325b7a88a946610b29d6a4e4d16c9
Brandon Ho <brandonho123@gmail.com>
2022-10-12 21:59:59 -0400

    Update sqlite db for local use as backup

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB --> local db 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")  # run locally as backup
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
